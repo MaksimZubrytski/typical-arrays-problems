3b5034de1578308395d103e2abeb4c759c25ee97
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-01-27 21:33:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0;
+    }
+    else {
+        let temp = array[0];
+        for(let i = 1; i < array.length; i++) {
+            if(array[i] < temp) {
+                temp = array[i]
+            }
+        }
+        return temp;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0;
+    }
+    else {
+        let temp = array[0];
+        for(let i = 1; i < array.length; i++) {
+            if(array[i] > temp) {
+                temp = array[i]
+            }
+        }
+        return temp;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0;
+    }
+    else {
+       let sum = array.reduce((acc, item) => (acc + item), 0);
+       return sum / array.length
+    }
 }
